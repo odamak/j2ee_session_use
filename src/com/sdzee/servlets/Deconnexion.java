0c368c9d32103bb7f7b54a948eeb8e59203d9edb
unknown <omardamak1@gmail.com>
2019-07-04 17:59:13 +0100

    Use redirect in log out to go to log in page and change url

--- src/com/sdzee/servlets/Deconnexion.java
+++ src/com/sdzee/servlets/Deconnexion.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 public class Deconnexion extends HttpServlet {
     public static final String URL_REDIRECTION = "http://www.siteduzero.com";
+    public static final String VUE = "/connexion";
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         /* Récupération et destruction de la session en cours */
@@ -17,6 +18,8 @@ public class Deconnexion extends HttpServlet {
         session.invalidate();
 
         /* Redirection vers le Site du Zéro ! */
-        response.sendRedirect( URL_REDIRECTION );
+//        response.sendRedirect( URL_REDIRECTION );
+        response.sendRedirect( "/session/connexion" );
+//        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 }
