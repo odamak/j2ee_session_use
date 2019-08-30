c416bd1329c2248acbe8d8ca4f678d36eb009b09
unknown <omardamak1@gmail.com>
2019-08-30 17:25:13 +0100

    Integrate DAO

---
+++ src/com/sdzee/dao/UtilisateurDao.java
@@ -0,0 +1,11 @@
+package com.sdzee.dao;
+
+import com.sdzee.beans.Utilisateur;
+
+public interface UtilisateurDao {
+
+    void creer( Utilisateur utilisateur ) throws DAOException;
+
+    Utilisateur trouver( String email ) throws DAOException;
+
+}
